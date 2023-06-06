b10578a2df40e42e907bfb5e1b43ae7db9a66202
iLouis <louisjiao@aliyun.com>
2023-06-06 11:20:16 +0800

    no message

--- backend/src/shared/protocols/serviceProto.ts
+++ backend/src/shared/protocols/serviceProto.ts
@@ -2,9 +2,6 @@ import { ServiceProto } from 'tsrpc-proto';
 import { MsgChat } from './MsgChat';
 import { ReqSend, ResSend } from './PtlSend';
 
-// This is a demo service proto file (auto generated)
-// Feel free to delete it
-
 export interface ServiceType {
     api: {
         "Send": {
@@ -18,14 +15,15 @@ export interface ServiceType {
 }
 
 export const serviceProto: ServiceProto<ServiceType> = {
+    "version": 4,
     "services": [
         {
-            "id": 0,
+            "id": 12,
             "name": "Chat",
             "type": "msg"
         },
         {
-            "id": 1,
+            "id": 13,
             "name": "Send",
             "type": "api"
         }
